805770c10c728a7ac3751a33332c0a73202873de
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2017-11-01 11:25:15 -0600

    20171101 CSC: test

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -11,7 +11,7 @@ namespace NetCore_Angular.Controllers
     {
         private static string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Cristian"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         [HttpGet("[action]")]

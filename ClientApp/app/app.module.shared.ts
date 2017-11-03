7a2f385f3df13cff39ee939f369d39e33b5449e6
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2017-11-03 11:31:49 -0600

    20171103 CSC: Uso de servico para leer datos

--- ClientApp/app/app.module.shared.ts
+++ ClientApp/app/app.module.shared.ts
@@ -11,6 +11,9 @@ import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
 
+
+import { MakeService } from './services/make.service';
+
 @NgModule({
     declarations: [
         AppComponent,
@@ -32,6 +35,8 @@ import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.com
             { path: 'vehicles/new', component: VehicleFormComponent },
             { path: '**', redirectTo: 'home' }
         ])
+    ],providers: [
+        MakeService
     ]
 })
 export class AppModuleShared {

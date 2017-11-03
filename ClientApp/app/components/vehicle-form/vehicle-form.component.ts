d1653033b58ea2af1d8f243b83392bc39f7fabb8
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2017-11-03 11:54:08 -0600

    20171103 CSC: Combos en cascada

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -8,7 +8,9 @@ import { MakeService } from '../../services/make.service';
 })
 export class VehicleFormComponent implements OnInit {
 
-  makes: any;
+  makes: any[];
+  models: any[];
+  vehicle: any = {};
 
   constructor(private makeService: MakeService) { }
 
@@ -16,11 +18,15 @@ export class VehicleFormComponent implements OnInit {
     this.makeService.getMakes().subscribe(
       makes => {
         this.makes = makes
-        console.log("MAKES : ", this.makes);
       }
     );
 
 
+  }
+
+  onMakeChange() {
+    var selectedMake = this.makes.find(m => m.id == this.vehicle.make);
+    this.models = selectedMake ? selectedMake.models : [];
   }
 
 }

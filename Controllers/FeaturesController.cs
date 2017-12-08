dbab0b6ce9c2f97b65eb96180e75d598e6ce098e
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2017-12-08 13:37:22 -0600

    20171208 CSC: Cambios KeyValuePairResource

--- Controllers/FeaturesController.cs
+++ Controllers/FeaturesController.cs
@@ -25,10 +25,10 @@ namespace NetCore_Angular.Controllers
         }
 
         [HttpGet("/api/features")]
-        public async Task<IEnumerable<FeatureResource>> GetFeatures()
+        public async Task<IEnumerable<KeyValuePairResource>> GetFeatures()
         {
             var features = await context.Features.ToListAsync();
-            return mapper.Map<List<Feature>, List<FeatureResource>>(features);
+            return mapper.Map<List<Feature>, List<KeyValuePairResource>>(features);
         }
 
     }
--- Controllers/Resources/KeyValuePairResource.cs
+++ Controllers/Resources/KeyValuePairResource.cs
@@ -1,15 +1,13 @@
-using NetCore_Angular.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 
-
 namespace NetCore_Angular.Controllers.Resources
 {
-    public class ModelResource
+    public class KeyValuePairResource
     {
+
         public int Id { get; set; }
         public string Name { get; set; }
     }
